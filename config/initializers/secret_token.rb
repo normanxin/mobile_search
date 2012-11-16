c755569dd2ac98fb354625a032eacda873e9440c
ZhipengLi <charrli@126.com>
2012-11-15 22:43:23 -0800

    initialized commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MobileSearch::Application.config.secret_token = 'a0d093c06a10216538a1026c23779c7f9c771a3e02d0cb432e32b715dd64cdf21e8b62b79e06958cc223c23e66705774256c94868f5c8783b90ee5db4d0cb0d9'
