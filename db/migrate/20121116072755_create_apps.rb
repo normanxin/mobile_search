cf479d0280c112e2ce8242bf7bbd4b010e09b708
ZhipengLi <charrli@126.com>
2012-11-17 22:54:15 -0800

    changed03

--- db/migrate/20121116072755_create_apps.rb
+++ db/migrate/20121116072755_create_apps.rb
@@ -2,7 +2,7 @@ class CreateApps < ActiveRecord::Migration
   def self.up
     create_table :apps do |t|
       t.string :name
-      t.text :description, :limit=>nil
+      t.text :description
       t.string :publisher
       t.string :image_url
       t.integer :counter
